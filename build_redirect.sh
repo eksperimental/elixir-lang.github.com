6a89c37a0e148df12817e7d24c93d8f76e752cb7
eksperimental <eksperimental@users.noreply.github.com>
2017-04-15 03:17:08 +0700

    Redirect index.html pages that were previously excluded

--- build_redirect.sh
+++ build_redirect.sh
@@ -2,11 +2,7 @@
 
 cd docs/
 printf -- "---\nlayout: redirect\n---\n" > ./redirect
-rename 's/index\.html/index\.html~/' index.html
-rename 's/index\.html/index\.html~/' **/index.html
 find ./ -name "*.html" | xargs -I {} sh -c "rm {}; ln ./redirect {}"
-rename 's/index\.html~/index\.html/' index.html~
-rename 's/index\.html~/index\.html/' **/index.html~
 
 cd ../
 bundle exec jekyll build
@@ -15,4 +11,4 @@ find _site/docs -name '*.html' | xargs -d '\n' -n 1 sed -e '/./,$!d' -i
 find _site/docs -type f | grep -vP "\.html$" | grep -v "_site/docs/README.md" | grep -v "_site/docs/logo.png" | xargs -d '\n' rm
 find _site/docs -type d -empty -delete
 
-# now doc are ready under: _site/docs/
+echo "Now docs are ready under: _site/docs/"
